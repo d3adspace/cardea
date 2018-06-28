bea0d34bd00d7958df671488e9c8daeee9b888dd
Felix Klauke <info@felix-klauke.de>
2018-06-28 12:56:45 +0200

    Add comments for pom explanation, add commons cli and bootstrap class for upcoming command line launcher.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ src/main/java/de/d3adspace/cardea/CardeaServerBootstrap.java
@@ -0,0 +1,11 @@
+package de.d3adspace.cardea;
+
+/**
+ * @author Felix Klauke <info@felix-klauke.de>
+ */
+public class CardeaServerBootstrap {
+
+    public static void main(String[] args) {
+
+    }
+}
